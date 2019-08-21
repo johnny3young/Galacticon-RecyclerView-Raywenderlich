d6f8bd2ff76eae476328fa6c85e0eb62cf4ce250
johnny3young <youngerblack@gmail.com>
2019-08-21 14:12:40 -0500

    Send project

---
+++ app/src/main/java/com/raywenderlich/galacticon/Extensions.kt
@@ -0,0 +1,14 @@
+package com.raywenderlich.galacticon
+
+import android.support.annotation.LayoutRes
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class Extensions {
+
+    fun ViewGroup.inflate(@LayoutRes layoutRes: Int, attachToRoot: Boolean = false): View {
+        return LayoutInflater.from(context).inflate(layoutRes, this, attachToRoot)
+    }
+
+}
